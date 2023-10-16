531e0e8abf8ee38ed974dfc01c319dc3476094d9
unknown <richiesenlia@gmail.com>
2023-10-16 13:38:15 +0700

    fix function

--- netlify/functions/pesan.js
+++ netlify/functions/pesan.js
@@ -2,10 +2,15 @@ const API_ENDPOINT = 'https://courageous-basbousa-3ef9e8.netlify.app/.netlify/fu
 
 export default async (request, context) => {
     let body = await request.json()
+    let sum = 0
+    for(let i = 0;i<body['harga'].length();i++){
+        sum = sum + body['harga'][i]
+    }
     console.log(body)
     console.log(body['harga'])
+    console.log(API_ENDPOINT+"?totalHarga="+sum)
   try {
-    const response = await fetch(API_ENDPOINT);
+    const response = await fetch(API_ENDPOINT+"?totalHarga="+sum);
     const data = await response.json();
     return Response.json({ data });
   } catch (error) {
