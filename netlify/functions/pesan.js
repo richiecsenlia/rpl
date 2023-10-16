dac9459944312199af1a79e15306717080fa4c45
unknown <richiesenlia@gmail.com>
2023-10-16 13:19:40 +0700

    fix function

--- netlify/functions/pesan.js
+++ netlify/functions/pesan.js
@@ -1,7 +1,8 @@
 const API_ENDPOINT = 'https://courageous-basbousa-3ef9e8.netlify.app/.netlify/functions/token';
 
 export default async (request, context) => {
-    console.log(request.body())
+    console.log(request)
+    console.log(request.body)
   try {
     const response = await fetch(API_ENDPOINT);
     const data = await response.json();
